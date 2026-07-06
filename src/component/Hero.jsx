122b1355b0437b534107df78afb1bb611b55488c
Gibson779 <79742592+Gibson779@users.noreply.github.com>
2026-07-06 14:24:46 +0100

    NewCommit

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,21 +1,35 @@
+import { Link } from "react-router-dom";
+
 export default function Hero() {
   return (
     <section className="hero">
       <img 
-        src="/images/home.jpg" 
+        src="/images/stock2.jpg" 
         alt="Home banner"
         className="hero__bg"
       />
 
       <div className="hero__content">
         <h1 className="hero_title">
-          Transforming Spaces With Detailed <br />
-          Landscapes
+         Premium Landscaping & <br/>
+         Driveway Transformations 
+
         </h1>
 
         <p>
-          Creating your dream gardens with qualty attention to detial
+         Creating beautiful outdoor spaces across Greater Manchester
         </p>
+
+         <div className="cta-buttons">
+          <div className="link">
+        <Link to="/contact">
+        <button className="btn btn-primary">Get A Free Quote</button>
+        </Link>
+        <Link to="/gallery">
+        <button className="btn btn-secondary">View Our Work</button>
+        </Link>
+        </div>
+        </div>
       </div>
     </section>
   );
